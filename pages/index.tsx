ab93f0633f14a394c72414db3f3b95da5bedb21c
viniciusoliveiras <vinitag190@gmail.com>
2021-05-13 17:48:26 -0300

    Autenticação com SSR | Recuperando token no server-side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
+
 import { AuthContext } from '../context/AuthContext';
 import styles from '../styles/Home.module.css';
 
@@ -44,3 +47,20 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (!cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
